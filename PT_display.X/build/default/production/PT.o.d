c4e7bdc75e2ec1860296e8592774ac50b1f7f458
Dialecteec <89866948+Dialecteec@users.noreply.github.com>
2021-12-09 20:04:30 -0500

    Add files via upload

---
+++ PT_display.X/build/default/production/PT.o.d
@@ -0,0 +1,14 @@
+build/default/production/PT.o: PT.c config.h pt_cornell_1_2_1.h \
+ tft_master.h tft_gfx.h debouncer.h ts_lcd.h
+
+config.h:
+
+pt_cornell_1_2_1.h:
+
+tft_master.h:
+
+tft_gfx.h:
+
+debouncer.h:
+
+ts_lcd.h:
