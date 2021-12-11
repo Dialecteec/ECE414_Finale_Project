6db2a93078bf3040f065da62ed44335263fd9ab9
Dialecteec <89866948+Dialecteec@users.noreply.github.com>
2021-12-11 17:26:55 -0500

    Delete test_touch_rev.X directory

--- test_touch_rev.X/build/default/production/Adafruit_2_4_LCD_Serial_Library/ts_lcd.o.d
+++
@@ -1,8 +0,0 @@
-build/default/production/Adafruit_2_4_LCD_Serial_Library/ts_lcd.o: \
- Adafruit_2_4_LCD_Serial_Library/ts_lcd.c \
- Adafruit_2_4_LCD_Serial_Library/ts_lcd.h \
- Adafruit_2_4_LCD_Serial_Library/../TouchScreen.h
-
-Adafruit_2_4_LCD_Serial_Library/ts_lcd.h:
-
-Adafruit_2_4_LCD_Serial_Library/../TouchScreen.h:
