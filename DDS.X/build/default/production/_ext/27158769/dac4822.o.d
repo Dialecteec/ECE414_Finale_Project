f1c31c66d778ef970a1d9b57c799c1d132cf9b9e
Dialecteec <89866948+Dialecteec@users.noreply.github.com>
2021-12-10 21:01:15 -0500

    Delete DDS.X directory

--- DDS.X/build/default/production/_ext/27158769/dac4822.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/27158769/dac4822.o: \
- ../DDS_Example/dac4822.c ../DDS_Example/dac4822.h
-
-../DDS_Example/dac4822.h:
--- DDS.X/build/default/production/_ext/27158769/past_lab_songs.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/27158769/past_lab_songs.o: \
- ../DDS_Example/past_lab_songs.c ../DDS_Example/notes.h
-
-../DDS_Example/notes.h:
