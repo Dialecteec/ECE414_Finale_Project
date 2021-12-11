fd0c751f4463cf7e8e2030781eae57f148d9b953
Dialecteec <89866948+Dialecteec@users.noreply.github.com>
2021-12-11 17:18:38 -0500

    Add files via upload

---
+++ test_touch_rev.X/build/default/production/Adafruit_2_4_LCD_Serial_Library/ts_lcd.o.d
@@ -0,0 +1,8 @@
+build/default/production/Adafruit_2_4_LCD_Serial_Library/ts_lcd.o: \
+ Adafruit_2_4_LCD_Serial_Library/ts_lcd.c \
+ Adafruit_2_4_LCD_Serial_Library/ts_lcd.h \
+ Adafruit_2_4_LCD_Serial_Library/../TouchScreen.h
+
+Adafruit_2_4_LCD_Serial_Library/ts_lcd.h:
+
+Adafruit_2_4_LCD_Serial_Library/../TouchScreen.h:
---
+++ test_touch_rev.X/build/default/production/touch_main_example.o.d
@@ -0,0 +1,17 @@
+build/default/production/touch_main_example.o: touch_main_example.c \
+ config.h adc_intf.h TouchScreen.h \
+ Adafruit_2_4_LCD_Serial_Library/tft_master.h \
+ Adafruit_2_4_LCD_Serial_Library/tft_gfx.h \
+ Adafruit_2_4_LCD_Serial_Library/ts_lcd.h
+
+config.h:
+
+adc_intf.h:
+
+TouchScreen.h:
+
+Adafruit_2_4_LCD_Serial_Library/tft_master.h:
+
+Adafruit_2_4_LCD_Serial_Library/tft_gfx.h:
+
+Adafruit_2_4_LCD_Serial_Library/ts_lcd.h:
