2565bb5c862697969a70827296feadadc7d79b48
Dialecteec <89866948+Dialecteec@users.noreply.github.com>
2021-12-11 15:22:44 -0500

    Add files via upload

---
+++ DDS.X/build/default/production/dds_test.o.d
@@ -0,0 +1,10 @@
+build/default/production/dds_test.o: dds_test.c pt_cornell_1_2_1.h \
+ config.h dds.h music.h
+
+pt_cornell_1_2_1.h:
+
+config.h:
+
+dds.h:
+
+music.h:
---
+++ DDS.X/music.h
@@ -0,0 +1,12 @@
+#ifndef MUSIC_H
+#define	MUSIC_H
+
+#include <stdint.h>
+
+#define notelen 29
+
+extern uint8_t Astley_note1[notelen];
+
+extern uint8_t Astley_dur1[notelen];
+
+#endif	/* MUSIC_H */
