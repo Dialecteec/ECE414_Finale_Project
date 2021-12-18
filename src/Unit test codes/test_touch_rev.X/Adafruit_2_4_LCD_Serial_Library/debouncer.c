e8c11bd6aeda815a5477a757cf66ca2d212a7ede
Dialecteec <89866948+Dialecteec@users.noreply.github.com>
2021-12-17 21:10:10 -0500

    Add files via upload

---
+++ src/Unit test codes/test_touch_rev.X/Adafruit_2_4_LCD_Serial_Library/debouncer.c
@@ -0,0 +1,60 @@
+/* 
+ * File:   debounce.h
+ * This module debounces the input from button by setting it to high
+ * after a press is detected
+ */
+#include <xc.h>
+#include <inttypes.h>
+
+static enum DBa_states {NPUSH, MAYPUSH, PUSHED, MAYNPUSH,DBa_SMStart} DBa_state;
+uint8_t pressed;
+void Init_DBa(){
+    DBa_state = DBa_SMStart;
+    pressed = 0x00;
+}
+
+void TickFct_DB(uint8_t btna)
+{
+    switch(DBa_state){
+        case DBa_SMStart:
+            DBa_state =  NPUSH;
+            pressed = 0;
+            break;
+        case NPUSH:
+            if (btna==0x01){
+                DBa_state = MAYPUSH;
+            } else if (btna==0x00){
+                DBa_state =  NPUSH;
+                pressed = 0;
+            } 
+            break;
+        
+        case MAYPUSH:
+            if (btna==0x01){
+                DBa_state = PUSHED;
+                pressed = 0x01;
+            } else if (btna==0x00){
+                DBa_state =  NPUSH;
+                pressed = 0;
+            } break;
+        
+        case PUSHED:
+            if (btna==0x01){
+                DBa_state = PUSHED;
+                pressed = 0x01;
+            } else if (btna==0x00){
+                DBa_state =  MAYNPUSH;
+            } 
+            break;
+        
+        case MAYNPUSH:
+            if (btna==0x01){
+                DBa_state = PUSHED;
+                pressed = 0x01;
+            } else if (btna==0x00){
+                DBa_state =  NPUSH;
+                pressed = 0;
+            } 
+            break;
+    }
+}
