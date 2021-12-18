6c21605bc4dc2dfd857b2d597f39d5add8c90489
Dialecteec <89866948+Dialecteec@users.noreply.github.com>
2021-12-17 21:09:03 -0500

    Add files via upload

---
+++ src/Unit test codes/DDS.X/build/default/production/dds.o.d
@@ -0,0 +1,5 @@
+build/default/production/dds.o: dds.c dds.h dac4822.h
+
+dds.h:
+
+dac4822.h:
---
+++ src/Unit test codes/DDS.X/notes.h
@@ -0,0 +1,58 @@
+/* 
+ * File:   notes.h
+ * Author: Ziqi Sun
+ *
+ * Created on September 20, 2017, 8:37 PM
+ */
+//
+//#ifndef NOTES_H
+//#define	NOTES_H
+
+#define R   0x0 
+#define A2  110 
+#define A2S 116.54 
+#define B2  123.47 
+#define C3  130.81 
+#define C3S 138.59 
+#define D3  146.83 
+#define D3S 155.56 
+#define E3  164.81 
+#define F3  174.61 
+#define F3S 185 
+#define G3  196 
+#define G3S 207.65 
+#define A3  220 
+#define A3S 233.1 
+#define B3  246.9 
+#define C4  261.6 
+#define C4S 277.2
+#define D4  293.7
+#define D4S 311.1
+#define E4  329.6
+#define F4  349.2
+#define F4S 370
+#define G4  392
+#define G4S 415.3
+#define A4  440
+#define A4S 466.2
+#define B4  493.9
+#define C5  523.3
+#define C5S 554.4
+#define D5  587.3
+#define D5S 622.2
+#define E5  659.2
+#define F5  698.4
+#define F5S 740.9
+#define G5  784
+#define G5S 830.6
+#define A5  880
+#define A5S 932.3
+#define B5  987.8
+#define C6  1046.5
+#define C6S 1108.73
+#define D6  1174.66
+#define D6S 1244.51
+#define E6  1318.51
+#define F6  1396.91
+#define F6S 1479.98
+#define G6  1567.98
