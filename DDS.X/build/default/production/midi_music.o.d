4f689342f196a16e47fcbe6ec18341f2d33f8d6c
Dialecteec <89866948+Dialecteec@users.noreply.github.com>
2021-12-17 21:05:58 -0500

    Delete DDS.X directory

--- DDS.X/build/default/production/midi_music.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/midi_music.o: midi_music.c midi_music.h
-
-midi_music.h:
--- DDS.X/build/default/production/songs.o.d
+++
@@ -1,7 +0,0 @@
-build/default/production/songs.o: songs.c notes.h midi_music.h songs.h
-
-notes.h:
-
-midi_music.h:
-
-songs.h:
